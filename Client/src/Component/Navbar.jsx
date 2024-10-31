4e23432e2ba9bcbaf399ecc849ce9110b3537b62
playDev11 <play.dev6@gmail.com>
2024-10-31 23:45:11 +0100

    Updated package-lock.json and package.json to include new dependencies, added react-icons and updated Navbar component with new logo and icons

--- Client/src/Component/Navbar.jsx
+++ Client/src/Component/Navbar.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import {HiMenuAlt4} from  'react-icons/hi'
+import {AiOutlineClose} from   'react-icons/ai'
+
+import logo from  '../../images/logo.png'
+
+
+
 
 const Navbar = () => {
   return (
-    <div className=''></div>
+    <nav className='w-full flex md:justify-center justify-between items-center p-4'>
+      <div className='md:flex-[0.5] flex-initial justify-center items-center'>
+        <img src={logo} alt=""  className='w-32 cursor-pointer'/>
+      </div>
+      <ul className='text-white md:flex hidden list-none flex-row justify-between items-center flex-initial'>
+
+      </ul>
+    </nav>
   )
 }
 
