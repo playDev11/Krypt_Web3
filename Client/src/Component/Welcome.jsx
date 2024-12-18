d3aa2cd346494e53187a44c36a10c45d85bf537d
playDev11 <play.dev6@gmail.com>
2024-12-18 12:05:49 +0100

    feat: edit card

--- Client/src/Component/Welcome.jsx
+++ Client/src/Component/Welcome.jsx
@@ -33,17 +33,14 @@ const Welcome = () => {
 
   const handleSubmit = () => {
     // const { addressTo, amount, keyword, message } = formData
-
     // e.preventDefault()
-
     // if (!addressTo || !amount || !keyword || !message) return
-
     // sendTransaction()
   }
 
   return (
     <div className="flex w-full justify-center items-center">
-      <div className="flex mf:flex-row flex-col items-start justify-between md:p-20 py-12 px-4">
+      <div className="flex mf:flex-row flex-row items-start justify-between gap-[300px] md:p-20 py-12 px-4">
         <div className="flex flex-1 justify-start items-start flex-col mf:mr-10">
           <h1 className="text-3xl sm:text-5xl text-white text-gradient py-1">
             Send Crypto <br /> across the world
@@ -52,18 +49,16 @@ const Welcome = () => {
             Explore the crypto world. Buy and sell cryptocurrencies easily on
             Krypto.
           </p>
-         
-            <button
-              type="button"
-              className="flex flex-row justify-center items-center my-5 bg-[#2952e3] p-3 rounded-full cursor-pointer hover:bg-[#2546bd]"
-            >
-              <AiFillPlayCircle className="text-white mr-2" />
-              <p className="text-white text-base font-semibold">
-                Connect Wallet
-              </p>
-            </button>
 
-          <div className="grid sm:grid-cols-3 grid-cols-2 w-full mt-10">
+          <button
+            type="button"
+            className="flex flex-row justify-center items-center my-5 bg-[#2952e3] p-3 rounded-full cursor-pointer hover:bg-[#2546bd]"
+          >
+            <AiFillPlayCircle className="text-white mr-2" />
+            <p className="text-white text-base font-semibold">Connect Wallet</p>
+          </button>
+
+          <div className="grid sm:grid-cols-2 grid-cols-2 w-full mt-10">
             <div className={`rounded-tl-2xl ${companyCommonStyles}`}>
               Reliability
             </div>
@@ -82,7 +77,7 @@ const Welcome = () => {
         </div>
 
         <div className="flex flex-col flex-1 items-center justify-start w-full mf:mt-0 mt-10">
-          <div className="p-3 flex justify-end items-start flex-col rounded-xl h-40 sm:w-72 w-full my-5 eth-card .white-glassmorphism ">
+          <div className="p-2 flex justify-end items-start flex-col rounded-xl h-48 sm:w-96 w-full my-5 eth-card .white-glassmorphism ">
             <div className="flex justify-between flex-col w-full h-full">
               <div className="flex justify-between items-start">
                 <div className="w-10 h-10 rounded-full border-2 border-white flex justify-center items-center">
@@ -91,9 +86,7 @@ const Welcome = () => {
                 <BsInfoCircle fontSize={17} color="#fff" />
               </div>
               <div>
-                <p className="text-white font-light text-sm">
-                  0x657jh.....343
-                </p>
+                <p className="text-white font-light text-sm">0x657jh.....343</p>
                 <p className="text-white font-semibold text-lg mt-1">
                   Ethereum
                 </p>
@@ -101,42 +94,20 @@ const Welcome = () => {
             </div>
           </div>
           <div className="p-5 sm:w-96 w-full flex flex-col justify-start items-center blue-glassmorphism">
-            <Input
-              placeholder="Address To"
-              name="addressTo"
-              type="text"
-            />
-            <Input
-              placeholder="Amount (ETH)"
-              name="amount"
-              type="number"
-              
-            />
-            <Input
-              placeholder="Keyword (Gif)"
-              name="keyword"
-              type="text"
-             
-            />
-            <Input
-              placeholder="Enter Message"
-              name="message"
-              type="text"
-              
-            />
+            <Input placeholder="Address To" name="addressTo" type="text" />
+            <Input placeholder="Amount (ETH)" name="amount" type="number" />
+            <Input placeholder="Keyword (Gif)" name="keyword" type="text" />
+            <Input placeholder="Enter Message" name="message" type="text" />
 
             <div className="h-[1px] w-full bg-gray-400 my-2" />
 
-            
-              
-              <button
-                type="button"
-                onClick={handleSubmit}
-                className="text-white w-full mt-2 border-[1px] p-2 border-[#3d4f7c] hover:bg-[#3d4f7c] rounded-full cursor-pointer"
-              >
-                Send now
-              </button>
-            
+            <button
+              type="button"
+              onClick={handleSubmit}
+              className="text-white w-full mt-2 border-[1px] p-2 border-[#3d4f7c] hover:bg-[#3d4f7c] rounded-full cursor-pointer"
+            >
+              Send now
+            </button>
           </div>
         </div>
       </div>
