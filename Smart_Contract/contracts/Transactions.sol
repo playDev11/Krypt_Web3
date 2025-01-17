e6b35d5edefd188399cf80e081e458e3084d4ffb
playDev11 <play.dev6@gmail.com>
2025-01-17 13:56:48 -0800

    feat: Update SPDX license identifier and modify event and function parameters in Transactions.sol

--- Smart_Contract/contracts/Transactions.sol
+++ Smart_Contract/contracts/Transactions.sol
@@ -1,11 +1,11 @@
-// SPDX license identifier: UNLICENSED
+// SPDX license identifier: MIT;
 
 pragma solidity ^0.8.0;
 
 contract Transactions {
     uint256 transactionsCount;
 
-    event Transfer(address from, address reciever, uint amount, string message, uint256 timestamp, string keyword);
+    event Transfer(address from, address reciever, uint amount, string message, uint256 timestamp, string keyword, string reciept);
 
 
 
@@ -20,17 +20,20 @@ contract Transactions {
 
     TransferStruct[] transactions;
 
-    function addToBlockChain(address payable receiver, uint amount,) public {
-        transactionCount +=1;
+    function addToBlockChain(address payable receiver, uint amount, string memory message, string memory keyword) public {
+        transactionsCount +=1;
+        transactions.push(TransferStruct(msg.sender, receiver, amount, message, block.timestamp, keyword));
+
+        emit Transfer(msg.sender, receiver, amount, message, block.timestamp, keyword, "reciept");
     
     }
 
     function getTransactions() public view returns (TransferStruct[] memory){
-        // return transactions;
+        return transactions;
         
     }
     function getTransactionsCount() public view returns (uint256){
-        // return transactionsCount;
+        return transactionsCount;
     }
 
 }
